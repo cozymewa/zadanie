a4ce27e67e700b33a05d320dc719a7de1bfadd70
1900134 <1900134@TE-GDA-D-D21E16>
2024-09-16 08:49:54 +0200

    Dodaj pliki projektów.

---
+++ zadanie/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace zadanie
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ zadanie/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace zadanie
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
